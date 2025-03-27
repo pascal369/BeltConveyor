1a7cebb69f7c2063516bf650c44a28afa1eb3b48
pascal369 <138413758+pascal369@users.noreply.github.com>
2025-03-27 15:35:54 +0900

    Add files via upload

---
+++ BltCvPulley.py
@@ -0,0 +1,354 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import FreeCAD as App
+import FreeCADGui as Gui
+from PySide import QtGui
+from PySide import QtUiTools
+from PySide import QtCore
+from FreeCAD import Base
+import FreeCAD, Part, math
+import DraftVecUtils
+import Sketcher
+import PartDesign
+from math import pi
+import Draft
+from . import paramPulley
+
+belt_haba=['400','450','500','600','700','750','800','900','1000',]
+pulley_buhin=['DrivePulley','HeadPulley','Take_upPulley','TailPulley','BendPulley','SnapPulley'
+]
+#プーリー寸法
+#ベルト幅   L     A    B    C    D    E     d1    d2   d3   t1  t2  t3  t4
+pulley_dim={
+'400':(   450,   50,  100, 500, 400,  100, 50,   45,  40,  9,  9,  8,  8),
+'450':(   500,   50,  100, 550, 400,  100, 50,   45,  40,  9,  9,  8,  8),
+'500':(   550,   50,  100, 600, 400,  100, 50,   45,  40,  9,  9,  8,  8),
+'600':(   650,   50,  100, 700, 400,  100, 50,   45,  40,  9,  9,  8,  8),
+'700':(   770,   50,  100, 820, 400,  100, 50,   45,  40,  9,  9,  8,  8),
+'750':(   820,   50,  100, 870, 400,  100, 50,   45,  40,  9,  9,  8,  8),
+'800':(   870,   50,  100, 920, 400,  100, 50,   45,  40,  9,  9,  8,  8),
+'900':(  1000,   50,  100,1050, 400,  100, 50,   45,  40,  9,  9,  8,  8),
+'1000':( 1100,   50,  100,1150, 400,  100, 50,   45,  40,  9,  9,  8,  8),
+}
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(400, 325)
+        Dialog.move(900, 0)
+        #ベルト幅
+        self.label = QtGui.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(11, 37, 61, 16))
+        self.label.setObjectName("label")
+        self.comboBox = QtGui.QComboBox(Dialog)
+        self.comboBox.setGeometry(QtCore.QRect(80, 37, 130, 20))
+        self.comboBox.setObjectName("comboBox")
+        #部品
+        self.comboBox_2 = QtGui.QComboBox(Dialog)
+        self.comboBox_2.setGeometry(QtCore.QRect(80, 11, 130, 20))
+        self.comboBox_2.setObjectName("comboBox_2")
+        self.label_2 = QtGui.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(11, 11, 61, 16))
+        self.label_2.setObjectName("label_2")
+        #L
+        self.label_30 = QtGui.QLabel(Dialog)
+        self.label_30.setGeometry(QtCore.QRect(50, 63, 61, 16))
+        self.label_30.setObjectName("label_3")
+        self.lineEdit_30 = QtGui.QLineEdit(Dialog)
+        self.lineEdit_30.setGeometry(QtCore.QRect(80, 63, 50, 20))
+        self.lineEdit_30.setObjectName("lineEdit")
+        #A
+        self.label_3 = QtGui.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(50, 93, 61, 16))
+        self.label_3.setObjectName("label_3")
+        self.lineEdit_3 = QtGui.QLineEdit(Dialog)
+        self.lineEdit_3.setGeometry(QtCore.QRect(80, 93, 50, 20))
+        self.lineEdit_3.setObjectName("lineEdit")
+        #B
+        self.label_6 = QtGui.QLabel(Dialog)
+        self.label_6.setGeometry(QtCore.QRect(50, 120, 61, 16))
+        self.label_6.setObjectName("label_6")
+        self.lineEdit_6 = QtGui.QLineEdit(Dialog)
+        self.lineEdit_6.setGeometry(QtCore.QRect(80, 120, 50, 20))
+        self.lineEdit_6.setObjectName("lineEdit_6") 
+        #C
+        self.label_50 = QtGui.QLabel(Dialog)
+        self.label_50.setGeometry(QtCore.QRect(50, 150, 61, 16))
+        self.label_50.setObjectName("label_6")
+        self.lineEdit_50 = QtGui.QLineEdit(Dialog)
+        self.lineEdit_50.setGeometry(QtCore.QRect(80, 150, 50, 20))
+        self.lineEdit_50.setObjectName("lineEdit_50")   
+        #D
+        self.label_7 = QtGui.QLabel(Dialog)
+        self.label_7.setGeometry(QtCore.QRect(50, 180, 61, 16))
+        self.label_7.setObjectName("label_7")
+        self.lineEdit_7 = QtGui.QLineEdit(Dialog)
+        self.lineEdit_7.setGeometry(QtCore.QRect(80, 180, 50, 20))
+        self.lineEdit_7.setObjectName("lineEdit_7")
+        #E
+        self.label_8 = QtGui.QLabel(Dialog)
+        self.label_8.setGeometry(QtCore.QRect(50, 210, 61, 16))
+        self.label_8.setObjectName("label_8")
+        self.lineEdit_8 = QtGui.QLineEdit(Dialog)
+        self.lineEdit_8.setGeometry(QtCore.QRect(80, 210, 50, 20))
+        self.lineEdit_8.setObjectName("lineEdit_8")
+        #d1
+        self.label_9 = QtGui.QLabel(Dialog)
+        self.label_9.setGeometry(QtCore.QRect(50, 240, 61, 16))
+        self.label_9.setObjectName("label_9")
+        self.lineEdit_9 = QtGui.QLineEdit(Dialog)
+        self.lineEdit_9.setGeometry(QtCore.QRect(80, 240, 50, 20))
+        self.lineEdit_9.setObjectName("lineEdit_9") 
+        #d2
+        self.label_10 = QtGui.QLabel(Dialog)
+        self.label_10.setGeometry(QtCore.QRect(140, 63, 61, 16))
+        self.label_10.setObjectName("label_10")
+        self.lineEdit_10 = QtGui.QLineEdit(Dialog)
+        self.lineEdit_10.setGeometry(QtCore.QRect(160, 63, 50, 20))
+        self.lineEdit_10.setObjectName("lineEdit_10") 
+        #d3
+        self.label_11 = QtGui.QLabel(Dialog)
+        self.label_11.setGeometry(QtCore.QRect(140, 90, 61, 16))
+        self.label_11.setObjectName("label_11")
+        self.lineEdit_11 = QtGui.QLineEdit(Dialog)
+        self.lineEdit_11.setGeometry(QtCore.QRect(160, 90, 50, 20))
+        self.lineEdit_11.setObjectName("lineEdit_11")
+        #t1
+        self.label_12 = QtGui.QLabel(Dialog)
+        self.label_12.setGeometry(QtCore.QRect(140, 120, 61, 16))
+        self.label_12.setObjectName("label_12")
+        self.lineEdit_12 = QtGui.QLineEdit(Dialog)
+        self.lineEdit_12.setGeometry(QtCore.QRect(160, 120, 50, 20))
+        self.lineEdit_12.setObjectName("lineEdit_12")
+        #t2
+        self.label_13 = QtGui.QLabel(Dialog)
+        self.label_13.setGeometry(QtCore.QRect(140, 150, 61, 16))
+        self.label_13.setObjectName("label_13")
+        self.lineEdit_13 = QtGui.QLineEdit(Dialog)
+        self.lineEdit_13.setGeometry(QtCore.QRect(160, 150, 50, 20))
+        self.lineEdit_13.setObjectName("lineEdit_13")  
+        #t3
+        self.label_14 = QtGui.QLabel(Dialog)
+        self.label_14.setGeometry(QtCore.QRect(140, 180, 61, 16))
+        self.label_14.setObjectName("label_14")
+        self.lineEdit_14 = QtGui.QLineEdit(Dialog)
+        self.lineEdit_14.setGeometry(QtCore.QRect(160, 180, 50, 20))
+        self.lineEdit_14.setObjectName("lineEdit_14")
+        #t4
+        self.label_15 = QtGui.QLabel(Dialog)
+        self.label_15.setGeometry(QtCore.QRect(140, 210, 61, 16))
+        self.label_15.setObjectName("label_15")
+        self.lineEdit_15 = QtGui.QLineEdit(Dialog)
+        self.lineEdit_15.setGeometry(QtCore.QRect(160, 210, 50, 20))
+        self.lineEdit_15.setObjectName("lineEdit_15")
+        #image
+        self.label_5 = QtGui.QLabel(Dialog)
+        self.label_5.setGeometry(QtCore.QRect(225, 0, 160, 270))
+        #self.label_5.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_5.setObjectName("label_5")
+
+        #実行
+        self.pushButton = QtGui.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(160, 295, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+
+        self.comboBox.addItems(belt_haba)
+        self.comboBox_2.addItems(pulley_buhin)
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+        self.comboBox.setCurrentIndex(1)
+        self.comboBox.currentIndexChanged[int].connect(self.onSpec)
+        self.comboBox.setCurrentIndex(0)
+
+        self.comboBox_2.setCurrentIndex(1)
+        self.comboBox_2.currentIndexChanged[int].connect(self.onSpec)
+        self.comboBox_2.setCurrentIndex(0)
+
+        QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL("pressed()"), self.create)
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "pully", None))
+        self.label_30.setText(QtGui.QApplication.translate("Dialog", "L", None))
+        self.label_3.setText(QtGui.QApplication.translate("Dialog", "A", None))
+        self.label_6.setText(QtGui.QApplication.translate("Dialog", "B", None))
+        self.label_50.setText(QtGui.QApplication.translate("Dialog", "C", None))
+        self.label_7.setText(QtGui.QApplication.translate("Dialog", "D", None))
+        self.label_8.setText(QtGui.QApplication.translate("Dialog", "E", None))
+        self.label_9.setText(QtGui.QApplication.translate("Dialog", "d1", None))
+        self.label_10.setText(QtGui.QApplication.translate("Dialog", "d2", None))
+        self.label_11.setText(QtGui.QApplication.translate("Dialog", "d3", None))
+        self.label_12.setText(QtGui.QApplication.translate("Dialog", "t1", None))
+        self.label_13.setText(QtGui.QApplication.translate("Dialog", "t2", None))
+        self.label_14.setText(QtGui.QApplication.translate("Dialog", "t3", None))
+        self.label_15.setText(QtGui.QApplication.translate("Dialog", "t4", None))
+        self.pushButton.setText(QtGui.QApplication.translate("Dialog", "create", None))
+    def onSpec(self):
+        global buhin
+        global B0
+        global L
+        global A
+        global B
+        global C
+        global D
+        global E
+        global d1
+        global d2
+        global d3
+        global t1
+        global t2
+        global t3
+        buhin=self.comboBox_2.currentText()
+        pic=buhin+'.png'
+        base=os.path.dirname(os.path.abspath(__file__))
+        joined_path = os.path.join(base, 'png_data',pic)
+        self.label_5.setPixmap(QtGui.QPixmap(joined_path))
+    
+        B0=self.comboBox.currentText()
+        sa=pulley_dim[B0]
+        L=float(sa[0])
+        A=float(sa[1])
+        C=float(sa[3])
+        if buhin=='DrivePulley' or buhin=='HeadPulley':
+            D=400.0
+        elif buhin=='TailPulley':
+            D=350.0
+        elif buhin=='Take_upPulley':    
+            D=380.0
+        elif buhin=='Bendpulley':  
+            D=250.0 
+        elif buhin=='SnapPulley': 
+            D=250.0 
+
+        if buhin=='DrivePulley':    
+            E=float(sa[5])
+        else:
+            E=0.0
+
+        d1=float(sa[6])
+        d2=float(sa[7])
+        if buhin=='DrivePulley':   
+            d3=float(sa[8])
+        else:
+            d3=0.0
+        t1=float(sa[9])
+        t2=float(sa[10])
+        t3=float(sa[11])
+        t4=float(sa[12])
+        B=round(d2*2.3,1)
+        E=round(1.7*d3,1)
+
+        self.lineEdit_30.setText(str(L))
+        self.lineEdit_3.setText(str(A))
+        self.lineEdit_6.setText(str(B))
+        self.lineEdit_50.setText(str(C))
+        self.lineEdit_7.setText(str(D))
+        self.lineEdit_8.setText(str(E))
+        self.lineEdit_9.setText(str(d1))
+        self.lineEdit_10.setText(str(d2))
+        self.lineEdit_11.setText(str(d3))
+        self.lineEdit_12.setText(str(t1))
+        self.lineEdit_13.setText(str(t2))
+        self.lineEdit_14.setText(str(t3))
+        self.lineEdit_15.setText(str(t4))
+    
+    def create(self):
+        print(buhin)
+        
+        label=buhin
+        try:
+            obj = App.ActiveDocument.addObject("Part::FeaturePython",label)
+        except:
+            doc=App.newDocument()
+            obj = App.ActiveDocument.addObject("Part::FeaturePython",label)
+
+        if label=='DrivePulley' :
+            L=float(self.lineEdit_30.text())
+            A=float(self.lineEdit_3.text())
+            B=float(self.lineEdit_6.text())
+            C=float(self.lineEdit_30.text())+50
+            D=float(self.lineEdit_7.text())
+            E=float(self.lineEdit_8.text())
+            d1=float(self.lineEdit_9.text())
+            d2=float(self.lineEdit_10.text())
+            d3=float(self.lineEdit_11.text())
+
+            t1=float(self.lineEdit_12.text())
+            t2=float(self.lineEdit_13.text())
+            t3=float(self.lineEdit_14.text())
+            t4=float(self.lineEdit_15.text())
+            
+            obj.addProperty("App::PropertyFloat", "L",label).L=L
+            obj.addProperty("App::PropertyFloat", "A",label).A=A
+            obj.addProperty("App::PropertyFloat", "B",label).B=B
+            obj.addProperty("App::PropertyFloat", "C",label).C=C
+            obj.addProperty("App::PropertyFloat", "D",label).D=D
+            obj.addProperty("App::PropertyFloat", "E",label).E=E
+            obj.addProperty("App::PropertyFloat", "d1",label).d1=d1
+            obj.addProperty("App::PropertyFloat", "d2",label).d2=d2
+            obj.addProperty("App::PropertyFloat", "d3",label).d3=d3
+            #obj.addProperty("App::PropertyFloat", "d4",label).d4=d4
+            obj.addProperty("App::PropertyFloat", "t1",label).t1=t1
+            obj.addProperty("App::PropertyFloat", "t2",label).t2=t2
+            obj.addProperty("App::PropertyFloat", "t3",label).t3=t3
+            obj.addProperty("App::PropertyFloat", "t4",label).t4=t4
+
+            paramPulley.Pulleys(obj)
+            obj.ViewObject.Proxy=0
+            FreeCAD.ActiveDocument.recompute() 
+
+            
+        elif label=='HeadPulley' or label=='Take_upPulley'or label=='BendPulley'\
+            or label=='SnapPulley':
+            L=float(self.lineEdit_30.text())
+            A=float(self.lineEdit_3.text())
+            B=float(self.lineEdit_6.text())
+            C=float(self.lineEdit_30.text())+50
+            D=float(self.lineEdit_7.text())
+            E=float(self.lineEdit_8.text())
+            d1=float(self.lineEdit_9.text())
+            d2=float(self.lineEdit_10.text())
+            d3=float(self.lineEdit_11.text())
+
+            t1=float(self.lineEdit_12.text())
+            t2=float(self.lineEdit_13.text())
+            t3=float(self.lineEdit_14.text())
+            t4=float(self.lineEdit_15.text())
+            
+            obj.addProperty("App::PropertyFloat", "L",label).L=L
+            obj.addProperty("App::PropertyFloat", "A",label).A=A
+            obj.addProperty("App::PropertyFloat", "B",label).B=B
+            obj.addProperty("App::PropertyFloat", "C",label).C=C
+            obj.addProperty("App::PropertyFloat", "D",label).D=D
+            obj.addProperty("App::PropertyFloat", "E",label).E=E
+            obj.addProperty("App::PropertyFloat", "d1",label).d1=d1
+            obj.addProperty("App::PropertyFloat", "d2",label).d2=d2
+            obj.addProperty("App::PropertyFloat", "d3",label).d3=d3
+            obj.addProperty("App::PropertyFloat", "t1",label).t1=t1
+            obj.addProperty("App::PropertyFloat", "t2",label).t2=t2
+            obj.addProperty("App::PropertyFloat", "t3",label).t3=t3
+            obj.addProperty("App::PropertyFloat", "t4",label).t4=t4
+            try:
+                doc=App.activeDocument()
+            except:
+                doc=App.newDocumento()    
+
+            paramPulley.Pulleys(obj)
+            obj.ViewObject.Proxy=0
+            FreeCAD.ActiveDocument.recompute()   
+        Gui.SendMsgToActiveView("ViewFit")    
+
+class main():
+        d = QtGui.QWidget()
+        d.ui = Ui_Dialog()
+        d.ui.setupUi(d)
+        d.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+        d.show()
+        # スクリプトのウィンドウを取得
+        script_window = Gui.getMainWindow().findChild(QtGui.QDialog, 'd')
+        # 閉じるボタンを無効にする
+        script_window.setWindowFlags(script_window.windowFlags() & ~QtCore.Qt.WindowCloseButtonHint)
+     
---
+++ paramMtrPly.py
@@ -0,0 +1,210 @@
+from FreeCAD import Base
+import FreeCAD, Part, math
+from math import pi
+import FreeCADGui as Gui
+import Draft
+import FreeCAD as App
+import Part
+from . import mtrply_data
+
+class MotorPly:
+    def __init__(self, obj):
+        self.Type = 'BasePlate'
+        obj.Proxy = self
+        App.activeDocument().recompute(None,True,True)        
+    def execute(self,obj):
+        label=obj.Name
+        key=App.ActiveDocument.getObject(label).key
+        A=App.ActiveDocument.getObject(label).A
+        a=App.ActiveDocument.getObject(label).a
+        b=App.ActiveDocument.getObject(label).b
+        D=App.ActiveDocument.getObject(label).D
+        D1=App.ActiveDocument.getObject(label).D1
+        D2=App.ActiveDocument.getObject(label).D2
+        D3=App.ActiveDocument.getObject(label).D3
+        L=App.ActiveDocument.getObject(label).L
+        C=App.ActiveDocument.getObject(label).C
+        E=App.ActiveDocument.getObject(label).E
+        M=App.ActiveDocument.getObject(label).M
+        R=App.ActiveDocument.getObject(label).R
+        N1=App.ActiveDocument.getObject(label).N1
+        N2=App.ActiveDocument.getObject(label).N2
+        d=App.ActiveDocument.getObject(label).d
+        B1=App.ActiveDocument.getObject(label).B1
+        B2=App.ActiveDocument.getObject(label).B2
+        F=App.ActiveDocument.getObject(label).F
+        G1=App.ActiveDocument.getObject(label).G1
+        G2=App.ActiveDocument.getObject(label).G2
+        H=App.ActiveDocument.getObject(label).H
+        P=App.ActiveDocument.getObject(label).P
+
+        x0=a+A/2
+
+        x4=float(H+G1-N1)
+        x5=P-(a+G1)
+        x6=P-(G2+b)    
+        
+        def drum(self):#駆動プーリー
+            global c00
+            #ドラム
+            p1=(-x0,0,0)
+            p2=(-x0,0,D2/2)
+            p3=(-x0+a,0,D2/2)
+            p4=(-x0+a,0,D1/2)
+            p5=(-x0+a+0.35*A,0,D/2)
+            p6=(-x0+a+0.65*A,0,D/2)
+            p7=(-x0+a+A,0,D1/2)
+            p8=(-x0+a+A,0,D3/2)
+            p9=(-x0+a+A+b,0,D3/2)
+            p10=(a+A+b-x0,0,0)
+            plst=[p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p1]
+            pwire=Part.makePolygon(plst)
+            #Part.show(pwire)
+            pface = Part.Face(pwire)
+            c00=pface.revolve(Base.Vector(0,0,0),Base.Vector(1,0,0),360)
+            #Part.show(c00)
+
+        def brg_left(self):
+            global c00
+            p1=(-x0,0,0)
+            p2=(-x0,0,R)
+            p3=(-x0,M/2-C,R)
+            p4=(-x0,M/2-C,R+5)
+            p5=(-x0,M/2-(C-5),R+5)
+            p6=(-x0,M/2-(C-5),C)
+            p7=(-x0,M/2,C+C-5)
+            p8=(-x0,M/2+C-5,C)
+            p9=(-x0,M/2+C-5,R+5)
+            p10=(-x0,M/2+C,R+5)
+            p11=(-x0,M/2+C,R)
+            p12=(-x0,M,R)
+            p13=(-x0,M,0)
+            edge1=Part.makeLine(p1,p2)
+            edge2=Part.makeLine(p2,p3)
+            edge3=Part.makeCircle(5,Base.Vector(p4),Base.Vector(1,0,0),-90,0)
+            edge4=Part.makeLine(p5,p6)
+            edge5=Part.Arc(Base.Vector(p6),Base.Vector(p7),Base.Vector(p8)).toShape()
+            #Part.show(edge5)
+            edge6=Part.makeLine(p8,p9)
+            edge7=Part.makeCircle(5,Base.Vector(p10),Base.Vector(1,0,0),180,270)
+            edge8=Part.makeLine(p11,p12)
+            edge9=Part.makeLine(p12,p13)
+            edge10=Part.makeLine(p13,p1)
+
+            aWire=Part.Wire([edge1,edge2,edge3,edge4,edge5,edge6,edge7,edge8,edge9,edge10])
+            #Part.show(aWire)
+            pface = Part.Face(aWire)
+            c00=pface.extrude(Base.Vector(-(N1),0,0))
+            c00.Placement=App.Placement(App.Vector(-x5,-M/2,-C),App.Rotation(App.Vector(1,1,1),0))
+            c01= Part.makeCylinder((C-15),x5,Base.Vector(-x5-x0,0,0),Base.Vector(1,0,0),360)
+            c00=c00.fuse(c01)
+            if x4>0.0 :
+                c00=c00.fuse(c01)
+                c02= Part.makeCylinder(0.8*(C-15),x4/3,Base.Vector(-(x5+N1+x4/3)-x0,0,0),Base.Vector(1,0,0),360)
+                c00=c00.fuse(c02)
+                c03= Part.makeCylinder(0.6*(C-15),x4/3,Base.Vector(-(x5+N1+2*x4/3)-x0,0,0),Base.Vector(1,0,0),360)
+                c00=c00.fuse(c03)
+                c04= Part.makeCylinder(0.4*(C-15),x4/3,Base.Vector(-(x5+N1+3*x4/3)-x0,0,0),Base.Vector(1,0,0),360)
+                c00=c00.fuse(c04)
+
+            #ボルト穴
+            for i in range(2):
+                if i==0:
+                    c05= Part.makeCylinder(d/2,C,Base.Vector(-(x5+N1)+G1-x0,E/2,-C),Base.Vector(0,0,1),360) 
+                else:
+                    c05= Part.makeCylinder(d/2,C,Base.Vector(-(x5+N1)+G1-x0,-E/2,-C),Base.Vector(0,0,1),360) 
+                c00=c00.cut(c05)
+            if key>10:
+                x7=N1-2*G1
+                for i in range(2):
+                    if i==0:
+                        c05= Part.makeCylinder(d/2,C,Base.Vector(-(x5+N1)+G1+x7-x0,E/2,-C),Base.Vector(0,0,1),360) 
+                    else:
+                        c05= Part.makeCylinder(d/2,C,Base.Vector(-(x5+N1)+G1+x7-x0,-E/2,-C),Base.Vector(0,0,1),360) 
+                        #c05= Part.makeCylinder(d/2,C,Base.Vector(-(x5)+G1+x7,-E/2,-C),Base.Vector(0,0,1),360)
+                    c00=c00.cut(c05)
+
+        def brg_Right(self):
+            global c00
+            p1=(-x0,0,0)
+            p2=(-x0,0,R)
+            p3=(-x0,M/2-C,R)
+            p4=(-x0,M/2-C,R+5)
+            p5=(-x0,M/2-(C-5),R+5)
+            p6=(-x0,M/2-(C-5),C)
+            p7=(-x0,M/2,C+C-5)
+            p8=(-x0,M/2+C-5,C)
+            p9=(-x0,M/2+C-5,R+5)
+            p10=(-x0,M/2+C,R+5)
+            p11=(-x0,M/2+C,R)
+            p12=(-x0,M,R)
+            p13=(-x0,M,0)
+            edge1=Part.makeLine(p1,p2)
+            edge2=Part.makeLine(p2,p3)
+            edge3=Part.makeCircle(5,Base.Vector(p4),Base.Vector(1,0,0),-90,0)
+            edge4=Part.makeLine(p5,p6)
+            edge5=Part.Arc(Base.Vector(p6),Base.Vector(p7),Base.Vector(p8)).toShape()
+            edge6=Part.makeLine(p8,p9)
+            edge7=Part.makeCircle(5,Base.Vector(p10),Base.Vector(1,0,0),180,270)
+            edge8=Part.makeLine(p11,p12)
+            edge9=Part.makeLine(p12,p13)
+            edge10=Part.makeLine(p13,p1)
+
+            aWire=Part.Wire([edge1,edge2,edge3,edge4,edge5,edge6,edge7,edge8,edge9,edge10])
+            pface = Part.Face(aWire)
+            c00=pface.extrude(Base.Vector(N2,0,0))
+            c00.Placement=App.Placement(App.Vector(a+A+b+x6,-M/2,-C),App.Rotation(App.Vector(0,0,1),0))
+            c01= Part.makeCylinder((C-15),x6,Base.Vector(a+A+b-x0,0,0),Base.Vector(1,0,0),360)
+            c00=c00.fuse(c01)
+            #ボルト穴
+            for i in range(2):
+                if i==0:
+                    c05= Part.makeCylinder(d/2,C,Base.Vector((a+A+x6+a)+G2-x0,E/2,-C),Base.Vector(0,0,1),360) 
+                else:
+                    c05= Part.makeCylinder(d/2,C,Base.Vector((a+A+x6+b)+G2-x0,-E/2,-C),Base.Vector(0,0,1),360) 
+                c00=c00.cut(c05)
+            if key>10:
+                x7=N2-2*G2
+                for i in range(2):
+                    if i==0:
+                        c05= Part.makeCylinder(d/2,C,Base.Vector((a+A+x6)+G2+x7-x0,E/2,-C),Base.Vector(0,0,1),360) 
+                    else:
+                        c05= Part.makeCylinder(d/2,C,Base.Vector((a+A+x6)+G2+x7-x0,-E/2,-C),Base.Vector(0,0,1),360) 
+                    c00=c00.cut(c05)  
+            #ケーブル
+            x8=a+A+b+N2+x6
+            p1=(x8-x0,0,0)
+            p2=(x8-x0,0,-15)  
+            p3=(x8-x0,0,0)
+            p4=(x8+15-x0,0,-15) 
+            p5=(x8+15-x0,0,-65)  
+            edge2 = Part.makeCircle(15, Base.Vector(p2), Base.Vector(0,1,0), -90, 0) 
+            edge3=Part.makeLine(p4,p5) 
+            aWire = Part.Wire([edge2,edge3])
+            edge4=Part.makeCircle(10, Base.Vector(x8-x0,0,0), Base.Vector(1,0,0), 0, 360) 
+            profile = Part.Wire([edge4])
+            makeSolid=True
+            isFrenet=True
+            c06 = Part.Wire(aWire).makePipeShell([profile],makeSolid,isFrenet)
+            c00=c00.fuse(c06)
+
+        drum(self) 
+        c1=c00 
+       
+        brg_left(self)
+        c2=c00
+        c1=c1.fuse(c2)
+        brg_Right(self)
+        c3=c00
+        c1=c1.fuse(c3)
+        c1.Placement=App.Placement(App.Vector(0,0,0),App.Rotation(App.Vector(0,0,1),90))
+        #Part.show(c1)
+        obj.Shape=c1
+        #Gui.SendMsgToActiveView("ViewFit")
+    
+
+
+
+        
+
+       
